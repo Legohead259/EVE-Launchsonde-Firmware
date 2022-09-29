9a260e29011ff1251277e46f4b1910454413525a
Braidan <legohead259@gmail.com>
2022-09-28 22:31:31 -0400

    Redefined pins according to Rev B4 schematic

--- src/helpers/pins.h
+++ src/helpers/pins.h
@@ -4,14 +4,18 @@
 
 #ifdef REVB4
     #define BATT_IN_PIN         A0
-    #define IGNITOR_CONT_PIN    A2
     #define IGNITE_PIN          A1
-    #define MODE_SELECT_PIN     13
+    #define BUZZER_PIN          A2
+    #define IGNITOR_CONT_PIN    A3
+    #define ALIVE_LED_PIN       A4
+    #define READY_LED_PIN       A5
+    #define ARMED_LED_PIN       2
+    #define RFM_RST_PIN         7
+    #define RFM_CS_PIN          9
+    #define RFM_IRQ_PIN         10
     #define GPS_PPS_INT_PIN     11
-    #define RFM_IRQ_PIN         7
-    #define RFM_CS_PIN          A5
-    #define RFM_RST_PIN         A4
+    #define SHT_RST_PIN         12
+    #define BNO_RST_PIN         13
     #define DOTSTAR_CLK_PIN     40
     #define DOTSTAR_DATA_PIN    41
-    #define SD_CS               A2
 #endif
